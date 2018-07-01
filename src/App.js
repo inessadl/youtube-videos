ac95b2af97206805011beddb4eabafb6f2b71930
Inessa Luerce <luerce.id@gmail.com>
2018-07-01 18:02:27 -0300

    config.js added as example

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 import './assets/SearchBar.css';
 import SearchBar from './components/SearchBar';
+import API_KEY from './.env/config.js';
 
 class App extends Component {
   render() {
+    console.log(API_KEY);
     return (
       <div className="App">
         <header className="App-header"></header>
         <SearchBar />
+
       </div>
     );
   }
