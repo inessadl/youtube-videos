82ea52f6f2b9627011ddbae602fb5417a8f5e25d
Inessa Luerce <luerce.id@gmail.com>
2018-07-02 13:43:38 -0300

    VideoList added

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import VideoList from './components/VideoList';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// ReactDOM.render(<App />, document.querySelector('.container'));
 
 registerServiceWorker();
