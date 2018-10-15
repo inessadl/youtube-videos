02b14a11f57a1510323475529005e03a5141f24d
UPASANA KOZYACHAYA <34530299+upasana-kozyachaya@users.noreply.github.com>
2018-10-15 14:20:32 -0400

    address PR comments

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,10 +4,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-      term: '',
-      placeholderText: 'Search',
-    };
+    this.state = { term: '' };
   }
 
   render() {
@@ -16,7 +13,7 @@ class SearchBar extends Component {
         <input
           value = { this.state.term }
           onChange = { event => this.onInputChange(event.target.value) }
-          placeholder = {this.state.placeholderText}
+          placeholder = 'Search'
         />
       </div>
     );
