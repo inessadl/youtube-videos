6d8065612ca838cfa234a0ab02dd58ec6134e4b9
inessa luerce <idluerce@thoughtworks.com>
2018-10-15 15:02:08 -0300

    Add placeholder for search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,6 +13,7 @@ class SearchBar extends Component {
         <input
           value = { this.state.term }
           onChange = { event => this.onInputChange(event.target.value)}
+          placeholder = "Search"
         />
       </div>
     );
