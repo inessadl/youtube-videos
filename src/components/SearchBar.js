ddd5866e6df6d746d75479487adff728fc312ccd
inessa luerce <idluerce@thoughtworks.com>
2018-10-17 16:44:46 -0300

    Update styles folder structure
    - Split css content in different files, one per component
    - Rename 'SimpleAppBar' component to 'AppBar'
    - Rename videos-app.css to 'AppBar.css'

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './styles/SearchBar.css'
 
 class SearchBar extends Component {
   constructor(props) {
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/VideoItem.css';
 
 const VideoItem = ({video, onVideoSelect}) => {
 
